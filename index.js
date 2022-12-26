b931af0a18dd78f4e6ec8f6a4b5624ed8026d761
vinaypatel1437 <66087753+vinaypatel1437@users.noreply.github.com>
2022-12-27 00:16:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+const PORT = process.env.PORT || 8080;
+const express = require("express")
+const cors = require('cors')
+const app = express();
+const show = require('./data/shows.js');
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/shows', (req, res) => {
+    console.log(show);
+    setTimeout(() => {
+        res.json({
+            message: "Successfully get data for " + req.query.date,
+            method: req.method,
+            url: req.url,
+            data: {
+                date: req.query.date,
+                theatres: show,
+            },
+        });
+    })
+});
+
+app.listen(PORT, () => {
+    console.log("running on Port 8080")
 })
-app.listen(process.env.PORT || 3000)
